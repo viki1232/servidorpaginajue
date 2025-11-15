4e5df8500e0bc896313846773a8d4c1122b80d25
viki1232 <nikollsalerno@gmail.com>
2025-11-14 19:55:29 -0500

    subiendo la carpeta game.js

---
+++ models/game.js
@@ -0,0 +1,40 @@
+// server/models/Game.js
+import mongoose from 'mongoose';
+
+const gameSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image_url: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model('Game', gameSchema);
